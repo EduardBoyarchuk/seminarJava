291a205490a49f9f27447861b374d480dfd5932e
EduardBoyarchuk <boyarchuk.eduard@list.ru>
2023-06-17 18:56:27 +0300

    seminar3/2

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import seminars.*;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        //------SeminarOne---------------
+        /*int varRandBite = SeminarOne.randBite();
+        System.out.println(varRandBite);
+
+        int varCount = SeminarOne.multiplesNumArray(varRandBite);
+        int varNotMultiples = SeminarOne.notMultiplesNumArray(varRandBite);
+        SeminarOne.showArray(varCount, varRandBite);
+        SeminarOne.showArrayNotMultiples(varNotMultiples, varRandBite);
+        System.out.println();*/
+        //-----SeminarTwo-------------------
+
+        //-----SeminarThree-----------------
+        SeminarThree obj = new SeminarThree();
+        obj.NewArList();
+
+        obj.OddNum();
+        obj.MinMaxElem();
+
+    }
+}
